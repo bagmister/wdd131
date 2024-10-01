7404c39c4ca0cb35b4f67a23148ac291273c3cdc
Kevin Bagley <bagmister@yahoo.com>
2024-09-30 22:51:40 -0600

    updates

--- scripts/temples.js
+++ scripts/temples.js
@@ -57,5 +57,20 @@ function populateGallery() {
       gallery.appendChild(figure);
   });
 }
+
+function lastModified(){
+  let lastModifiedDate = new Date(document.lastModified);
+  document.querySelector("#lastModified").innerHTML = lastModifiedDate.toLocaleString()
+}
+
+function currentYear() {
+  const currentYearValue = new Date().getFullYear();
+  document.querySelector("#currentyear").innerHTML = currentYearValue;
+}
+
+
+window.addEventListener('load',currentYear)
+window.addEventListener('load',lastModified)
+
 populateGallery();
 
