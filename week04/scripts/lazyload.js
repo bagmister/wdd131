a628c64bc2ac14e988d4c70453fc0b41e9e9750f
Kevin Bagley <bagmister@yahoo.com>
2024-10-10 00:37:41 -0600

    week4 updates

--- week04/scripts/lazyload.js
+++ week04/scripts/lazyload.js
@@ -8,9 +8,8 @@ function currentYear() {
   document.querySelector("#currentyear").innerHTML = currentYearValue;
 }
 
+
 window.addEventListener('load',currentYear)
 window.addEventListener('load',lastModified)
 
-const computeWindChill = function(windSpeed, temperature) {
-  return 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
-};
+populateGallery();
